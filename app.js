31abaafc286bbdcaa0932a13f01ae9652032bfc1
Rawand Dargali <rdargali@yahoo.com>
2020-06-29 15:12:31 -0500

    register

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+//mustache init
+const mustacheExpress = require("mustache-express");
+app.engine("mustache", mustacheExpress());
+app.set("views", "./views");
+app.set("view engine", "mustache");
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
