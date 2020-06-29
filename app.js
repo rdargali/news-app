badc1d0286c11d7668366c42f0828fc08a3bf97a
Rawand Dargali <rdargali@yahoo.com>
2020-06-29 15:40:09 -0500

    post routes and body parser

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+//body parser
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+
 //mustache init
 const mustacheExpress = require("mustache-express");
 app.engine("mustache", mustacheExpress());
@@ -10,6 +14,16 @@ app.set("view engine", "mustache");
 
 app.get("/register", (req, res) => {
   res.render("register");
+});
+
+app.post("/register", (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+
+  console.log(username);
+  console.log(password);
+
+  res.send("success");
 });
 
 app.listen(PORT, () => {
