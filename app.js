6154857913d243009836055201fd53052bd302f9
Rawand Dargali <rdargali@yahoo.com>
2020-06-29 16:29:24 -0500

    posting user cred to database

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+//postgres promise
+const pgp = require("pg-promise")();
+const CONNECTION_STRING = "postgres://localhost:5432/newsdb";
+const db = pgp(CONNECTION_STRING);
+
 //body parser
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 
-//mustache init
+//mustache
 const mustacheExpress = require("mustache-express");
 app.engine("mustache", mustacheExpress());
 app.set("views", "./views");
@@ -20,10 +25,21 @@ app.post("/register", (req, res) => {
   let username = req.body.username;
   let password = req.body.password;
 
-  console.log(username);
-  console.log(password);
-
-  res.send("success");
+  db.oneOrNone("SELECT userid FROM users WHERE username = $1", [username]).then(
+    (user) => {
+      if (user) {
+        res.render("register", { message: "Username already exists" });
+      } else {
+        //insert user into users table
+        db.none("INSERT INTO users(username, password) VALUES($1, $2)", [
+          username,
+          password,
+        ]).then(() => {
+          res.send("success");
+        });
+      }
+    }
+  );
 });
 
 app.listen(PORT, () => {
